638ac55db399fa178a95bb014c42880016d09b06
Medric Riley <medricr@gmail.com>
2019-11-14 14:54:49 -0800

    added basic CRUD routes for user schema on backend, added proxy to frontent package.json

--- client/models/UserSchema.js
+++ client/models/UserSchema.js
@@ -4,7 +4,8 @@ const schema = mongoose.Schema;
 const Schema = new schema({
 
 	username: {type: String},
-	password: {type: String}
+	password: {type: String}, 
+	bio: {type: String}
 });
 
 const User = mongoose.model("User", Schema);
--- server.js
+++ server.js
@@ -6,35 +6,61 @@ const mongoose = require("mongoose");
 const PORT = "3000";
 const testRoutes = express.Router();
 
+// Reference to database
 let Schema = require('./client/models/UserSchema.js');
+
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(testRoutes);
 
 // Establish connection to database
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+// Anything following localhost/ can be changed to fit whatever naming convention fits the project best 
+mongoose.connect('mongodb://localhost/todos', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 // Check connection to db
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
+connection.once('open', function () {
+	console.log("MongoDB database connection established successfully");
 })
 
 // Spin up server
-app.listen(PORT, function(){
+app.listen(PORT, function () {
 	console.log("Server is running on port " + PORT);
 });
 
-testRoutes.route('/test').post(function(req,res){
+// CRUD ROUTES - WILL MOVE TO SEPERATE FILE / DIRECTORY WHEN COMPLETE
+// CREATE
+testRoutes.route('/test').post(function (req, res) {
 	let test = new Schema;
-	test.username = "test";
+	test.username = "testing";
 
 	test.save()
-	  .then(test => {
-		res.status(200).json()
-	  })
-	  .catch(err => {
-		  res.status(400).send();
-	  })
+		.then(test => {
+			res.status(200).json(test + " inserted ")
+		})
+		.catch(err => {
+			res.status(400).send();
+		})
+})
+
+// READ
+testRoutes.route('/get').get(function (req, res) {
+	Schema.find()
+		.then(data => res.json(data))
+		.catch(err => res.status(422).json(err))
+})
+
+// UPDATE
+testRoutes.route('/update').post(function(req,res) {
+	Schema.updateOne({username: "testing"}, {bio: "tested"})
+		.then(data => res.json(data))
+		.catch(err => res.status(422).json(err))
+})
+
+// DELETE
+testRoutes.route('/delete').post(function(req,res) {
+	Schema.deleteOne({username: "testing"})
+		.then(data => res.json(data))
+		.catch(err => res.status(422).json(err));
 })
